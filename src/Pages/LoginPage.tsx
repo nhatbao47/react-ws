2a288b7a98a903b2c4b71d62ba517444f15532d6
David Nguyen <nhatbao@gmail.com>
2022-02-09 08:42:30 +0700

    Check login

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,19 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { fakeAuth } from "../Services/CommonService";
+import './LoginPage.css';
 
 const LoginPage = () => {
-    let navigate = useNavigate();
+    const navigate = useNavigate();
+    const [username, SetUsername] = useState('');
+    const [password, SetPassword] = useState('');
+    const [showMessage, SetShowMessage] = useState(false);
+    const adminUser = 'admin';
 
-    function handleClick() {
-        fakeAuth.isAuthenticated = true;
-        navigate('/dashboard');
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        if (username === adminUser && password === '123456') {
+            fakeAuth.isAuthenticated = true;
+            fakeAuth.username = adminUser;
+            navigate('/dashboard');
+        } else {
+            SetShowMessage(true);
+        }
     }
 
     return (
-        <div className="unauthorized-layout">
-            <h1>Login Page</h1>
-            <button onClick={() => handleClick()}>Login</button>
+        <div className="row">
+            <div className="col-md-4"></div>
+            <div className="col-md-4">
+                <form className="login-form" onSubmit={(e) => handleSubmit(e)}>
+                    <div className="img-container">
+                        <img src="/img/avatar.png" alt="Avatar" className="avatar" />
+                    </div>
+                    <div className="container">
+                        <label htmlFor="username"><b>Username</b></label>
+                        <input type="text" placeholder="Enter username" name="username" onChange={(e) => SetUsername(e.target.value)} required />
+                        <label htmlFor="password"><b>Password</b></label>
+                        <input type="password" placeholder="Enter password" name="password" onChange={(e) => SetPassword(e.target.value)} required />
+                        {showMessage && (
+                            <div className="text-danger">
+                                Your username or password is not correct
+                            </div>
+                        )}
+                        <button type="submit">Login</button>
+                        <p className="hint">Test user: <i>admin/123456</i></p>
+                    </div>
+                </form>
+            </div>
+            <div className="col-md-4"></div>
         </div>
     );
 }
--- src/Services/CommonService.js
+++ src/Services/CommonService.js
@@ -1,3 +1,4 @@
 export const fakeAuth = {
-    isAuthenticated: false
+    isAuthenticated: false,
+    username: ''
 };
